0c216b935a6f37a2034d6ce1e34fe1044972a61f
isak9975 <isak9975@gmail.com>
2025-11-26 22:23:52 +0900

    [김이삭]커밋

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
 import express from "express"
 import {setupApp} from "./config/appConfig.js"
-
+import NoticeRoute from "./routes/NoticeRoute.js"
 /*기본적인 서버 세팅*/
 
 //익스프레스 앱 생성
 const app = express();
 
-//라우트 호출
+//기본설정
 setupApp(app); 
+//라우트 호출
+app.use("/api/notice",NoticeRoute)
 
 //서버 포트 설정
 const port = 5000;
 
-//루트 경로에 대한 GET 요청 처리
+//express 포트 처리
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -3,10 +3,5 @@ import express from "express"
 /// 기본적인 서버 세팅 ///
 export function setupApp(app){
     app.use(express.json());
-
-    app.use("/api", (req, res) => {
-        res.send("API Root");
-    });
-
     return app;
 }
